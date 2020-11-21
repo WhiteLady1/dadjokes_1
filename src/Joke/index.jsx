659f850a74e346b85d79b83541b716f16be5ebf1
WhiteLady1 <megan15963@gmail.com>
2020-11-21 16:18:06 +0100

    15. úkol

---
+++ src/Joke/index.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const Joke = (props) => {
+  const [upLikes, setUpLikes] = useState(props.likes);
+  const [downLikes, setDownLikes] = useState(props.dislikes);
+
+  return (
+    <>
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src={`assets/img/${props.id}.png`} />
+            <p className="user-name">{props.name}</p>
+          </div>
+
+          <p className="joke__text">{props.text}</p>
+        </div>
+        <div className="joke__likes">
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={() => setUpLikes(upLikes + 1)}
+          ></button>
+          <span id="likes-up" className="likes-count likes-count--up">
+            {upLikes}
+          </span>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={() => setDownLikes(downLikes + 1)}
+          ></button>
+          <span id="likes-down" className="likes-count likes-count--down">
+            {downLikes}
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Joke;
